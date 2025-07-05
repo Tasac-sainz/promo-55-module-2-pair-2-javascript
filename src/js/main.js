de5dd985942ceaa8ed3a558863b591f2a0a0adec
tasac-sainz <tssainzdeaja@gmail.com>
2025-07-05 10:18:17 +0200

    ejericios 1, 2 y 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+document.querySelector ('h1').innerHTML ='Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente';
+
+document.querySelector ('p').innerHTML = "Hola, mundo";
+
+document.querySelector ('h2').innerHTML = "La Adalaber seleccionada es:"
+
+document.querySelector ('ul').innerHTML ="<li>Tania</li>"
+
