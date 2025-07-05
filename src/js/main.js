161003ab865f4f047b758121e5e2dc95bc73b86a
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-07-05 13:37:15 +0200

    inicio eventos

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ document.querySelector('h2').innerHTML = "La Adalaber seleccionada es:";
 document.querySelector('ul').innerHTML = "<li>Tania</li>";
 
 document.querySelector(".title").innerHTML = "lorem ipsum";
-document.querySelector(".image").src = "http://via.placeholder.com/350x150";
+document.querySelector(".image").src = "https://images.unsplash.com/photo-1600357077527-930ccbaf7773?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8";
 document.querySelector('.paragraph').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
 
 
@@ -21,4 +21,12 @@ document.querySelector(".address").innerHTML = myAddress;
 
 document.querySelector(".button")
 document.querySelector(".button").classList.add("disabled")
+
+let textInput = document.querySelector(".input");
+textInput.addEventListener("change", (event) => {console.log ("Estas escribiendo en el input");
+    textInput = event.target.value;
+    console.log ("valor del input dentro de la función", textInput);
+    document.querySelector('.hello').innerHTML = "Hola! "+event.target.value;
+});
+
 
