1457b4dae97d3b9f7a7d17fca29381b73f3d3bf3
tasac-sainz <tssainzdeaja@gmail.com>
2025-07-08 21:40:34 +0200

    dos ejercicios de condicionales

--- src/js/main.js
+++ src/js/main.js
@@ -52,3 +52,15 @@ buttonSubmit.addEventListener("click", (event) => {
         document.querySelector('.welcome').innerHTML = "Lo siento no estás registrado";
     }
 });
+const number = 0;
+if (number <1 && number > -1) {
+  console.log('El número es 0');
+} else if (number <=0) {
+  console.log('El número es negativo');
+} else if (number +2 >13 && number +2 <=20) {
+  console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
+} else if (number >20 && number <50) {
+  console.log('El número es mayor que 20 pero menor que 50')
+} else (number !==123123125);  {
+  console.log('el número no es 123123125')
+}
